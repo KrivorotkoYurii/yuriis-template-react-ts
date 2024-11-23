1450f8697b8d388b68a6906b9bdc9eae2590a098
Yurii Kryvorotko <krivorotkokrivorotko2016@gmail.com>
2024-11-23 18:33:16 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss';
+import './styles/main.scss';
+import styles from './styles/App.module.scss';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <div>Hello</div>
-    </div>
+    <section className={styles.container}>
+      <div className={styles.firstDiv}>Centered div</div>
+    </section>
   );
 };
